1562faf1dd136d10a604b9831ffaf457bab0e330
Kresimir Ivkovic <kivkovic@pm.me>
2019-03-30 14:29:38 +0100

    Replacing spawn with node-pty

--- server.js
+++ server.js
@@ -1,112 +1,64 @@
 const os = require('os');
 const fs = require('fs');
-const spawn = require('child_process').spawn;
+//const spawn = require('child_process').spawn;
+const pty = require('node-pty');
 const mime = require('mime');
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.use(express.static('/'))
 app.use(cors());
 
 process.on('warning', e => console.warn(e.stack));
 
-function spawnInstance (path) { // https://github.com/nodejs/help/issues/1183?s_tact=C209148W#issuecomment-376414424
-  const c = spawn(path);
-  return (command, literal = false) => {
-    return new Promise((resolve, reject) => {
-      var buf = Buffer.alloc(0);
-      c.stdout.on('data', (d) => {
-        buf = Buffer.concat([buf, d]);
-        resolve(String(buf));
-      })
-      c.stderr.on('data', d => {
-        buf = Buffer.concat([buf, d]);
-        reject(String(buf));
-      });
-      c.stdin.write(literal ? command : `${command} && echo PWD=$PWD && echo ERRORCODE=$?\n`);
-    })
-  }
+let cli = spawnInstance('bash');
+//cli('echo $PWD'); // TODO throw if this cause errors
+//const SIGINT = Symbol('SIGINT');
+
+let socket;
+
+function spawnInstance(path) {
+  const child = pty.spawn(path, [], {
+    name: 'xterm-color',
+    cols: 80,
+    rows: 30,
+    cwd: process.env.HOME,
+    env: process.env
+  });
+
+  child.on('data', data => {
+    console.log('sending chunk', JSON.stringify(data));
+    if (socket) socket.emit('consoleout', { data: JSON.stringify(data) });
+  });
+
+  return (command) => {
+    child.write(`${command} && echo ___PWD=$PWD && echo ___RETURNCODE=$?\n`); // add & to force run in bg and avoid interactive apps? lastline contains pid
+  };
 }
 
-async function executeBashCmd(command, forceLiteral = false, forceUSLocale = true) {
+async function executeBashCmd(command, forceUSLocale = true) {
     try {
-        if (forceLiteral) {
-            return { response: await cli(command, true), returncode: 0 };
-        } else {
-            const result = await cli((forceUSLocale ? 'LC_ALL=en_US.utf8 ' : '') + command);
-            const matches = result.match(/(?:^|\n)PWD=([^\n]+)\nERRORCODE=([0-9]+)\n*$/);
-            const response = result.replace(/(?:^|\n)PWD=([^\n]+)\nERRORCODE=([0-9]+)\n*$/, '');
-            return { response, returncode: matches ? matches[2] : -1, pwd: matches ? matches[1] : null };
-        }
+      return await cli((forceUSLocale ? 'LC_ALL=en_US.utf8 ' : '') + command);
     } catch (error) {
-        return { response: error, returncode: -1, pwd: null };
+      return { response: error, returncode: 1, pwd: null };
     }
-}
-
-const cli = spawnInstance('bash');
-
-async function processBashRequest(request, response, message) {
-  let result = await executeBashCmd(decodeURI(request.query.cmd));
-
-  if (result.returncode * 1 > 0) {
-    message.error = result.response;
-    message.pwd = result.pwd;
-  } else {
-    message.out = result.response;
-    message.pwd = result.pwd;
-  }
-  message.end = +new Date();
-  response.send(message);
 }
 
 app.get('/bash', async function(request, response) {
   const message = { start: +new Date() };
+  executeBashCmd(decodeURI(request.query.cmd));
+  /* if (request.query.cmd) {
+    processBashRequest(request, response, message);
+  } else if (request.query.autocomplete) {
+    //message.response = await executeBashCmd(decodeURI(request.query.autocomplete), false)
+    //response.send(response);
+  } else if (request.query.kill) {
 
-  if (request.query && request.query.autocomplete) {
-      const partial = decodeURI(request.query.autocomplete);
-      //const result = await executeBashCmd(`echo ${partial}$'\\t'$'\\t' | bash -i 2>&1 | head -n -4 | tail -n +2\n`, true);
-      const result = await executeBashCmd(`echo ${partial}$'\\t'$'\\t' | bash -i 2>&1 | tail -n +2\n`, true);
-
-      if (!result.response.match(/(Display all \d+ possibilities|not found, did you mean)/)) {
-          const list = result.response
-            .split(/[^\n]+:/)[0]
-            .split(/[ \n]+/)
-            .map(s => s.trim())
-            .filter(s => s && s != partial)
-            .sort((a, b) => a < b ? -1 : a > b ? 1 : 0);
-
-          message.out = { response: list, returncode: result.returncode };
-      } else {
-          message.out = { response: [], returncode: -1 };
-      }
-      response.send(message);
-
-    } else if (request.query && request.query.cmd) {
-        if (request.query.cmd.match(/^sudo/)) {
-          console.log('Received SUDO command', decodeURI(request.query.cmd));
-
-          const sudo = spawn('sudo', ['-S', 'ls']);
-
-            sudo.stderr.on('data', async function (data) {
-
-              console.log('Requesting sudo password', 'Answering:', decodeURI(request.query.password));
-              sudo.stdin.write(decodeURI(request.query.password) + '\n');
-              processBashRequest(request, response, message);
-            });
-
-            sudo.stdout.on('data', async function (data) {
-              processBashRequest(request, response, message);
-            });
-
-      } else {
-          console.log('Received command', decodeURI(request.query.cmd));
-          processBashRequest(request, response, message);
-      }
-  } else {
-      message.error = 'Empty command';
-      response.send(message);
-  }
+  } */
+  response.send({ ...message });
 });
 
 app.get('/stats', function(request, response) {
@@ -173,4 +125,10 @@ app.get('/typeof', async function (request, response) {
   response.send(message);
 });
 
-app.listen(1337);
+io.on('connection', sock => {
+  socket = sock;
+  console.log('socket connection from', sock.client.conn.remoteAddress);
+  sock.emit('connected');
+});
+
+http.listen(1337);
